4fbb8f44ce0d41965bede10ebbc005b21ca0bd70
Javier Fernández <jfernandez@joifilabs.com>
2021-03-29 13:31:56 +0200

    Modifies project name, description and README
    Also adds some comments in bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# I prefer to use poetry instead pipenv
 #source "$(pipenv --venv)"/bin/activate
 source "$(poetry env info --path)"/bin/activate
 
+# I prefer to run the manin.py directly and running the flask app in there by
+# running the app.run() method
 #export FLASK_APP=./src/main.py
 #flask run -h 0.0.0.0
 python -m src.main
